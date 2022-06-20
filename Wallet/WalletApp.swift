d772e2728fdf3371f940982fc2fbacd8f75a74ed
Fuad Adetoro <fuad@inhaus.ai>
2022-06-20 02:28:38 +0100

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Fuad on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
